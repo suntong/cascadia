e9bd569ed3f9dff12ac84c647618c5f59f6dea79
Tong Sun <suntong@cpan.org>
2017-03-19 18:35:10 -0400

    - [!] fix template & fix space

--- cascadiaDefs.go
+++ cascadiaDefs.go
@@ -16,9 +16,9 @@ import (
 
 type rootT struct {
 	cli.Helper
-	Filei *clix.Reader `cli:"*i,in"usage:"The html/xml file to read from (or stdin)"`
-	Fileo *clix.Writer `cli:"*o,out"usage:"The output file (or stdout)"`
-	CSS   string       `cli:"*c,css"usage:"CSS selectors"`
+	Filei *clix.Reader `cli:"*i,in" usage:"The html/xml file to read from (or stdin)"`
+	Fileo *clix.Writer `cli:"*o,out" usage:"The output file (or stdout)"`
+	CSS   string       `cli:"*c,css" usage:"CSS selectors"`
 }
 
 var root = &cli.Command{
