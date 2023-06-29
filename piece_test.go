7a5fade23300497eadeda17ed1f6228d41d0e92b
Tong Sun <suntong@cpan.org>
2023-06-29 12:21:02 -0400

    - [#] cosmetic update

--- piece_test.go
+++ piece_test.go
@@ -6,28 +6,28 @@ import (
 
 func TestPiece(t *testing.T) {
 	testCases(t, "Piece raw html", []testCase{
-		{"piece_raw_a",
-			[]string{"-i", "opt_piece_attr.html", "-o", "-c", "ul > li", "-p", "Source=RAW:a"},
-		},
+		{"piece_raw_a", []string{
+			"-i", "opt_piece_attr.html", "-o", "-c", "ul > li", "-p", "Source=RAW:a",
+		}},
 	})
 	testCases(t, "Piece Attributes", []testCase{
-		{"id",
-			[]string{"-i", "opt_piece_attr.html", "-o", "-c", "li > a", "-p", "id=ATTR:id"},
-		},
-		{"href",
-			[]string{"-i", "opt_piece_attr.html", "-o", "-c", "li > a", "-p", "url=ATTR:href"},
-		},
-		{"both_fields",
-			[]string{"-i", "opt_piece_attr.html", "-o", "-c", "li > a",
-				"-p", "id=ATTR:id", "-p", "url=ATTR:href"},
-		},
+		{"id", []string{
+			"-i", "opt_piece_attr.html", "-o", "-c", "li > a", "-p", "id=ATTR:id",
+		}},
+		{"href", []string{
+			"-i", "opt_piece_attr.html", "-o", "-c", "li > a", "-p", "url=ATTR:href",
+		}},
+		{"both_fields", []string{"-i", "opt_piece_attr.html", "-o", "-c", "li > a",
+			"-p", "id=ATTR:id", "-p", "url=ATTR:href",
+		}},
 
-		{"noexist",
-			[]string{"-i", "opt_piece_attr.html", "-o", "-c", "li > a", "-p", "NoExist=ATTR:noexist"},
-		},
-		{"script_src",
-			[]string{"-i", "opt_piece_script.html", "-o", "-c", "html > head > script", "-p", "SourceJS=ATTR:src"},
-		},
+		{"noexist", []string{
+			"-i", "opt_piece_attr.html", "-o", "-c", "li > a", "-p", "NoExist=ATTR:noexist",
+		}},
+
+		{"script_src", []string{
+			"-i", "opt_piece_script.html", "-o", "-c", "html > head > script", "-p", "SourceJS=ATTR:src",
+		}},
 		//{},
 	})
 }
